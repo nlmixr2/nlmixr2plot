63fc07ea51604324339c46b1483541f719631374
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-04-19 08:23:59 -0500

    Simply use reexports

--- R/reexports.R
+++ R/reexports.R
@@ -1,29 +1,15 @@
-# Consider if these should move to this package, too.
-
 #' @importFrom rxode2 geom_cens
 #' @export
-#' @rdname geom_cens
-#' @name geom_cens
-#' @title Put shaded regions where censoring occurs  (re-exported from `rxode2`)
-#' @inherit rxode2::geom_cens
 rxode2::geom_cens
 
 #' @importFrom rxode2 geom_amt
 #' @export
-#' @title Put vertical lines where dosing occurs (re-exported from `rxode2`)
-#' @rdname geom_amt
-#' @name geom_amt
-#' @inherit rxode2::geom_amt
 rxode2::geom_amt
 
 #' @importFrom rxode2 stat_cens
 #' @export
-#' @rdname geom_cens
-#' @name stat_cens
 rxode2::stat_cens
 
 #' @importFrom rxode2 stat_amt
 #' @export
-#' @rdname geom_amt
-#' @name stat_amt
 rxode2::stat_amt
